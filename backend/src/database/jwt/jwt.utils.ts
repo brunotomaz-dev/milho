c43ab0243c4e93867fefd416a8002b8868e31ddc
brunotomaz-dev <tomaz.bruno@gmail.com>
2023-07-05 13:00:07 -0300

    :sparkles: New Feat: AuthService
    Criada autenticação de usuário e tokem para o mesmo.

---
+++ backend/src/database/jwt/jwt.utils.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import * as jwt from 'jsonwebtoken';
+import * as errors from 'restify-errors';
+import IUser from '../interface/IUser';
+
+export const createToken = (user: IUser): string => {
+  const jwtKey = process.env.JWT_SECRET;
+  const token = jwt.sign(
+    {
+      email: user.email,
+      name: user.name,
+      role: user.role,
+    },
+    jwtKey as string,
+    {
+      expiresIn: '5h',
+      algorithm: 'HS256',
+    },
+  );
+  return token;
+};
+
+export const verifyToken = (token: string): IUser => {
+  const jwtKey = process.env.JWT_SECRET;
+  try {
+    const decoded = jwt.verify(token, jwtKey as string);
+    return decoded as IUser;
+  } catch (error) {
+    throw new errors.UnauthorizedError('Invalid token');
+  }
+};
